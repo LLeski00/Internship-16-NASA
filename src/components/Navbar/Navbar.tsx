c43d7995181c1961de1e8478b232e245e35494c6
Luka <llesko00@fesb.hr>
2025-03-03 16:05:30 +0100

    Implement navigation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+import "@/components/Navbar/Navbar.css";
+
 const Navbar = () => {
-    return <nav></nav>;
+    return (
+        <nav>
+            <h1>NASA Explore</h1>
+            <div className="navbar-links">
+                <Link to="/">Home</Link>
+                <Link to="/apod">APOD</Link>
+                <Link to="/mars-rover">Mars Rover</Link>
+                <Link to="/earth-imagery">Earth Imagery</Link>
+                <Link to="/neo">NEO</Link>
+                <Link to="/details">Details</Link>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
